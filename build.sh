d5d5efa01936dce711742984e0f7ff45a33ccae2
Satyajit Biswal <bsatyajit669@gmail.com>
2025-05-28 16:02:48 +0530

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Install system dependencies
-apt-get update
-apt-get install -y tesseract-ocr libtesseract-dev
-
-# Install Python dependencies
-pip install -r requirements.txt
